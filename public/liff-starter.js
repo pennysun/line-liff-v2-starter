8040c3c4a1bf0bdbae6879f354a25fdeacbc3afb
Penny <penny@TW-LW11271-M.local>
2019-11-05 00:18:23 +0800

    modified:   public/liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -125,8 +125,39 @@ function registerButtonHandlers() {
             sendAlertIfNotInClient();
         } else {
             liff.sendMessages([{
-                'type': 'text',
-                'text': "You've successfully sent a message! Hooray!"
+                "type": "template",
+                "altText": "This is a buttons template",
+                "template": {
+                    "type": "buttons",
+                    "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+                    "imageAspectRatio": "rectangle",
+                    "imageSize": "cover",
+                    "imageBackgroundColor": "#FFFFFF",
+                    "title": "Menu",
+                    "text": "Please select",
+                    "defaultAction": {
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "http://example.com/page/123"
+                    },
+                "actions": [
+                    {
+                        "type": "postback",
+                        "label": "Buy",
+                        "data": "action=buy&itemid=123"
+                    },
+                    {
+                        "type": "postback",
+                        "label": "Add to cart",
+                        "data": "action=add&itemid=123"
+                    },
+                    {
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "http://example.com/page/123"
+                    }
+                    ]
+                }
             }]).then(function() {
                 window.alert('Message sent');
             }).catch(function(error) {
